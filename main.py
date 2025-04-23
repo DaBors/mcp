884053faca7d7d8ec65e1a335c9426f5799607bf
Marton Borsos <borsos.marton95@gmail.com>
2025-04-23 23:11:36 +0200

    Added gcs resources

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from mcp.server.fastmcp import FastMCP
-from fastapi import FastAPI
-import uvicorn
-from os import environ
+from fastapi import FastAPI, HTTPException
+from google.cloud import storage
 
 # Create an MCP server
 mcp = FastMCP("Demo")
+bucket_name = "mcp-demo"
 
-# Add an addition tool
-@mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+@mcp.resource("file://list")
+def file_list() -> list[str]:
+    """List all files in the bucket"""
+    storage_client = storage.Client()
 
-# Add a dynamic greeting resource
-@mcp.resource("greeting://{name}")
-def get_greeting(name: str) -> str:
-    """Get a personalized greeting"""
-    return f"Hello, {name}!"
+    blobs = storage_client.list_blobs(bucket_name, prefix="texts/")
+
+    files = [blob.name.replace("texts/", "") for blob in blobs if not blob.name.endswith("/")]
+    return sorted(files)
+
+@mcp.resource("file://text/{filename}")
+def file(filename: str) -> str:
+    """Read a file"""
+    storage_client = storage.Client()
+
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(f"texts/{filename}")
+
+    if not blob.exists():
+        raise HTTPException(status_code=404, detail="File not found")
+
+    return blob.download_as_string().decode("utf-8")
 
 app = FastAPI()
 app.mount("/mcp", mcp.sse_app())
